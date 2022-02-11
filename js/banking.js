2af3fae02fb99d0770e6464c10b68178fbe78c71
zishan344 <zishanahmed344@gmail.com>
2022-02-11 10:02:21 +0600

    done the normal design

--- js/banking.js
+++ js/banking.js
@@ -19,3 +19,26 @@ document
     //clear input filed
     depositeInput.value = "";
   });
+
+document
+  .getElementById("withdrow-button")
+  .addEventListener("click", function () {
+    const withdrowINput = document.getElementById(" withdrow-input");
+    const withdrowAmountText = withdrowINput.value;
+    const withdrowAmount = parseFloat(withdrowAmountText);
+
+    // update withdrowAmount
+    const withdrowTotal = document.getElementById("withdrow-total");
+    const previousWithdrowTotalTExt = withdrowTotal.innerText;
+    const previousWithdrowTotal = parseFloat(previousWithdrowTotalTExt);
+    withdrowTotal.innerText = previousWithdrowTotal + withdrowAmount;
+
+    //update balanceTotal
+    const balanceTotal = document.getElementById("balance-total");
+    const balanceTotalText = balanceTotal.innerText;
+    const previouseBalanceTotal = parseFloat(balanceTotalText);
+    balanceTotal.innerText = previouseBalanceTotal - withdrowAmount;
+
+    //clear input filed
+    withdrowINput.value = "";
+  });
